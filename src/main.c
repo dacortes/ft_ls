6f229c92b928a33908cba6490444a7af054606d9
Your Name <you@example.com>
2025-05-23 15:38:42 +0200

    [ADD] update libft submodule and refactor directory reading logic in main.c

--- src/main.c
+++ src/main.c
@@ -33,42 +33,6 @@
 #include <string.h>
 #include <sys/stat.h>
 
-// short	loop_recursive(t_flags flags, t_stack *stack)
-// {
-// 	t_node	*curr;
-// 	t_node	*add;
-// 	DIR		*dir;
-// 	short	status;
-
-// 	(void)flags;
-// 	status = EXIT_SUCCESS;
-// 	dir = NULL;
-// 	while (stack->size > 0)
-// 	{
-// 		curr = copy_node(stack, true);
-// 		ft_printf("file name %s\n", curr->entry->d_name);
-// 		status = init_struct_dirent(&curr, dir, curr->entry->d_name);
-// 		if (status == 2)
-// 			stack->status = status;
-// 		while (status != end_dir)
-// 		{
-// 			if (default_directories(curr->entry->d_name, curr->entry->d_type))
-// 				continue ;
-// 			if (curr->entry->d_type == DT_DIR)
-// 			{
-// 					add = ft_calloc(1, sizeof(t_node));
-// 					if (!add)
-// 						exit(error_msg(MALLOC, 1, "exec_recursive_flag", ""));
-// 					status = init_struct_dirent(&add, dir, add->entry->d_name);
-// 					push_stack(stack, add);
-// 					ft_printf("+++++++++++++++++++++++++\n");
-// 			}
-// 		}
-// 		free(curr->entry);
-// 		free(curr);
-// 	}
-// 	return (true);
-// }
 
 DIR	*init_dir(char *file)
 {
@@ -88,58 +52,96 @@ short	is_readdir(struct dirent **entry, DIR		*dir)
 	return (true);
 }
 
+int	read_dir_entries(DIR *dir, struct  dirent ***entries)
+{
+	struct dirent	*entry;
+	struct dirent	**tmp;
+	int				i;
+
+	i = 0;
+	while (is_readdir(&entry, dir))
+	{
+		tmp = ft_realloc(*entries, sizeof(struct  dirent *) * i, sizeof(struct  dirent *) * (i + 1));
+		if (!tmp)
+			exit(error_msg(MALLOC, 1, "read_dir_entries", ""));
+		*entries = tmp;
+		// ft_printf("---*****%p\n", *entries);
+		copy_struct_dirent(entry, &(*entries)[i]);
+		i++;
+	}
+	return (i);
+}
+
 short	loop_recursive(t_flags flags, t_stack *stack)
 {
-	t_node	*curr;
-	t_node	*add;
-	DIR		*dir;
-	struct dirent *entry;
+	struct dirent	**entries;
+	t_node			*curr;
+	t_node			*add;
+	DIR				*dir;
+	int				entry_count;
+	int				i;
 
 	(void)flags;
+	entry_count = 0;
+	entries = NULL;
 	while (stack->size > 0)
 	{
 		curr = copy_node(stack, true);
 		ft_printf("Exploring: %s\n", curr->entry->d_name);
-
 		dir = init_dir(curr->entry->d_name);
 		if (!dir)
 			continue ;
-
-		while (is_readdir(&entry, dir))
+		entry_count = read_dir_entries(dir, &entries);
+		//ordenar todo si tiene flags
+		i = 0;
+		while (i < entry_count)
 		{
-			if (default_directories(entry->d_name, entry->d_type))
+			if (default_directories(entries[i]->d_name, entries[i]->d_type))
+			{
+				++i;
 				continue;
-
-			char *tmp = ft_addend_char(curr->entry->d_name, '/');
-			char *full_path = ft_strjoin(tmp, entry->d_name);
+			}
+			char *tmp;
+			int len = ft_strlen(curr->entry->d_name);
+			if (curr->entry->d_name[len] != '/')
+				tmp = ft_addend_char(curr->entry->d_name, '/');
+			else
+				tmp = ft_strdup(curr->entry->d_name);
+			char *full_path = ft_strjoin(tmp, entries[i]->d_name);
 			if (tmp)
 				free(tmp);
-			if (entry->d_type == DT_DIR)
+			if (entries[i]->d_type == DT_DIR)
 			{
 				add = ft_calloc(1, sizeof(t_node));
 				if (!add)
 					exit(error_msg(MALLOC, 1, "loop_recursive", ""));
-				// copy_struct_dirent(entry, &add->entry);
 				add->entry = ft_calloc(1, sizeof(struct dirent) + strlen(full_path) + 1);
                 ft_strcpy(add->entry->d_name, full_path);
 				push_stack(stack, add);
-				ft_printf("Added directory to stack: %s\n", entry->d_name);
+				// ft_printf("Added directory to stack: %s\n", entries[i]->d_name);
 			}
 			else
 			{
-				ft_printf("File: %s\n", entry->d_name);
+				ft_printf("File: %s\n", entries[i]->d_name);
 			}
 			if (full_path)
 				free(full_path);
+			i++;
 		}
-
+		i = 0;
+		while (i < entry_count)
+		{
+			free(entries[i]);
+			i++;
+		}
 		closedir(dir);
 		free(curr->entry);
 		free(curr);
 	}
+	if (entries)
+		free(entries);
 	return (true);
 }
-
 
 short	exec_recursive_flag(t_flags flags, char *start_dir)
 {
